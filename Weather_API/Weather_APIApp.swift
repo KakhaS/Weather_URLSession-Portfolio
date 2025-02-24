817ca1c72ea069b4f39e58562e92bd61abcabdd2
Kakha Sepashvili <kakhasepashvili@Kakhas-MacBook-Pro.local>
2025-02-24 20:13:02 +0400

    startin of project

---
+++ Weather_API/Weather_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_APIApp.swift
+//  Weather_API
+//
+//  Created by Kakha Sepashvili on 2/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Weather_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
